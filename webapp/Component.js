9a5562bff90f9cb8d584e12b1a227cbe8bf73014
Florian Zimmermann <f.zimmermann@neusta.de>
2026-04-22 13:45:42 +0000

    Version 1.0 Budget Card

---
+++ webapp/Component.js
@@ -0,0 +1,16 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("nes.zovpbudget.Component", {
+            metadata: {
+                manifest: "json"
+            },
+            init: function () {
+                console.log("Version 1.01 22.04.2026/15:41Uhr");
+                Component.prototype.init.apply(this, arguments);
+            }
+        });
+    }
+);
