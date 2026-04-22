de31b25edeae042cbab0a5a9be8ec55da7cc31a3
Florian Zimmermann <f.zimmermann@neusta.de>
2026-04-22 14:02:08 +0000

    Add global filter log

--- webapp/Component.js
+++ webapp/Component.js
@@ -9,6 +9,7 @@ sap.ui.define(
             },
             init: function () {
                 console.log("Version 1.01 22.04.2026/15:41Uhr");
+                console.log("Feature Global Filter aktiv");
                 Component.prototype.init.apply(this, arguments);
             }
         });
