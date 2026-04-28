53b6e099bf93d3fe0ea336ff6efdbb3c354a7f02
Florian Zimmermann <f.zimmermann@neusta.de>
2026-04-28 13:33:22 +0000

    New Cards

--- webapp/Component.js
+++ webapp/Component.js
@@ -8,7 +8,7 @@ sap.ui.define(
                 manifest: "json"
             },
             init: function () {
-                console.log("Version 1.01 22.04.2026/15:41Uhr");
+                console.log("Version 1.01 24.04.2026/08:04Uhr");
                 console.log("Feature Global Filter aktiv");
                 Component.prototype.init.apply(this, arguments);
             }
